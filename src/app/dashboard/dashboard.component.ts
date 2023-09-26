902b659b59279eb42ca20b29499e7269c0b9779a
Exaudi2003 <104402572+Exaudi2003@users.noreply.github.com>
2023-09-26 11:03:41 +0700

    update terbaru

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+// dashboard.component.ts
+
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var $: any; // Deklarasikan jQuery
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const menuItems = document.querySelectorAll(".menu-item");
+
+    menuItems.forEach(function (menuItem) {
+      const subMenu = menuItem.querySelector(".sub-menu");
+
+      menuItem.addEventListener("click", function (e) {
+        e.preventDefault();
+        if (subMenu) {
+          subMenu.classList.toggle("show-sub-menu");
+        }
+      });
+    });
+
+    const sidebar = document.querySelector('.sidebar');
+    const navbar = document.querySelector('.navbar');
+
+    const sidebarCollapse = document.getElementById('sidebarCollapse');
+
+    if (sidebar && navbar && sidebarCollapse) {
+      sidebarCollapse.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+        navbar.classList.toggle('active');
+      });
+    }
+  }
 }
